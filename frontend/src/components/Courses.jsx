6b1ef22507c9bfd70df78cf54153644f5c51ab21
Chaitanya Goratela <officialchaitanya22@gmail.com>
2025-09-25 12:40:39 +0530

    Added loader and error messages for browser page when render backend is down

--- frontend/src/components/Courses.jsx
+++ frontend/src/components/Courses.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { BookOpen, Users } from "lucide-react";
+import { BookOpen, Loader, Users } from "lucide-react";
 
 const Courses = () => {
   const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -16,13 +18,35 @@ const Courses = () => {
         setCourses(res.data.courses);
       } catch (err) {
         console.error("Failed to fetch courses", err);
+        setError(true);
+      } finally {
+        setLoading(false);
       }
     };
     fetchCourses();
   }, []);
 
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center text-gray-600">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (error || courses.length === 0) {
+    return (
+      <div className="min-h-screen flex items-center justify-center text-center px-4">
+        <p className="text-lg font-medium text-red-600">
+          Couldn’t fetch courses. The backend might be down (might take upto
+          2minutes).
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <div className="min-h-screen bg-gradient-to-b from-white via-blue-50 to-blue-100 py-10 px-4 sm:px-10">
+    <div className="min-h-screen bg-gradient-to-b from-white via-blue-100 to-blue-200 py-10 px-4 sm:px-10">
       <h2 className="text-4xl font-bold text-center text-gray-800 mb-10">
         Explore Courses
       </h2>
--- frontend/src/pages/Browse.jsx
+++ frontend/src/pages/Browse.jsx
@@ -4,7 +4,7 @@ import UserSidebarDrawer from "../components/UserDrawer";
 
 function Browse() {
   return (
-    <div className="bg-gradient-to-br from-slate-800 to-slate-900 overflow-hidden shadow-xl border-slate-700">
+    <div className="min-h-screen bg-gradient-to-b from-white via-blue-100 to-blue-300 py-10 px-4 sm:px-10">
       <Courses />
     </div>
   );
